cda8cb87baa3f7795eeb9e2efa392c3008ca6fa2
Keaglezzz <keaganthome@gmail.com>
2023-05-12 22:59:39 +0200

    cart modal to be

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,18 +1,24 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
 import axios from 'axios';
 
-// import getStripe from '../lib/getStripe';
-
 const Cart = (props) => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove, clearCart } = useStateContext();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [deliveryOption, setDeliveryOption] = useState("pickup"); // default to pickup
+  const [region, setRegion] = useState("");
+  const [deliveryAddress, setDeliveryAddress] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
   const getPriceForSelectedVolume = (item) => {
     if (item && item.selectedVolume && item.selectedVolume.price) {
@@ -24,7 +30,7 @@ const Cart = (props) => {
   };
 
   const generateDescription = () => {
-    return cartItems
+    let description = cartItems
       .map(
         (item) =>
           `${item.name} - ${
@@ -34,80 +40,72 @@ const Cart = (props) => {
           } x ${item.quantity}`
       )
       .join(', ');
-  };
   
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.YocoSDK) {
-      const yoco = new window.YocoSDK({
-        publicKey: 'pk_test_d5670e23vnkJO8Na29d4',
-      });
-  
-      const handleYocoPayment = async () => {
-        setShowCart(false); // Add this line to close the cart
-      
-        yoco.showPopup({
-          amountInCents: totalPrice * 100,
-          currency: 'ZAR',
-          name: 'Your Store or Product',
-          description: generateDescription(),
-          callback: async function (result) {
-            if (result.error) {
-              const errorMessage = result.error.message;
-              setShowCart(true); // Reopen the cart on an unsuccessful payment
-              toast.error("Error occurred: " + errorMessage + ". Please try again.");
-            } else {
-              try {
-                const response = await axios.post('/api/charge', {
-                  token: result.id,
-                  amountInCents: totalPrice * 100,
-                });
-          
-                // Handle successful payment
-                clearCart(); // Clear the cart on a successful payment
-                toast.success('Payment successful!');
-                // Clear the cart and redirect the user, or show a success message.
-              } catch (error) {
-                setShowCart(true); // Reopen the cart on an unsuccessful payment
-                toast.error("Error occurred: " + error.message + ". Please try again.");
-              }
-            }
-          },          
-        });
-      };
-      
-      const checkoutButton = document.querySelector('#checkout-button');
-      if (checkoutButton) {
-        checkoutButton.addEventListener('click', handleYocoPayment);
-      }
-  
-      return () => {
-        if (checkoutButton) {
-          checkoutButton.removeEventListener('click', handleYocoPayment);
-        }
-      };
+    // Add user info to description
+    description += ` Customer: ${name}, ${email}, ${contactNumber}. `;
+    if (deliveryOption === "delivery") {
+      description += `Delivery to ${deliveryAddress}, ${region}.`;
+    } else {
+      description += "In-store pickup.";
     }
-  }, [totalPrice]);
   
-  // const handleCheckout = async () => {
-  //   const stripe = await getStripe();
+    return description;
+  };
 
-  //   const response = await fetch('/api/stripe', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(cartItems),
-  //   });
+  const handleYocoPayment = async () => {
+    setShowCart(false); 
+      
+    new window.YocoSDK({
+      publicKey: 'pk_test_d5670e23vnkJO8Na29d4',
+    }).showPopup({
+      amountInCents: totalPrice * 100,
+      currency: 'ZAR',
+      name: 'Your Store or Product',
+      description: generateDescription(),
+      callback: async function (result) {
+        if (result.error) {
+          const errorMessage = result.error.message;
+          setShowCart(true); // Reopen the cart on an unsuccessful payment
+          toast.error("Error occurred: " + errorMessage + ". Please try again.");
+        } else {
+          try {
+            const response = await axios.post('/api/charge', {
+              token: result.id,
+              amountInCents: totalPrice * 100,
+            });
+      
+            // Handle successful payment
+            clearCart(); // Clear the cart on a successful payment
+            toast.success('Payment successful!');
+            // Clear the cart and redirect the user, or show a success message.
+          } catch (error) {
+            setShowCart(true); // Reopen the cart on an unsuccessful payment
+            toast.error("Error occurred: " + error.message + ". Please try again.");
+          }
+        }
+      },          
+    });
+  };
+      
+  const checkoutButton = document.querySelector('#checkout-button');
+  if (checkoutButton) {
+    checkoutButton.addEventListener('click', () => {
+      // Show the modal for user information when the checkout button is clicked
+      setShowModal(true);
+    });
+  }
 
-  //   if(response.statusCode === 500) return;
-    
-  //   const data = await response.json();
+  const modalVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
 
-  //   toast.loading('Redirecting...');
+  const handleModalSubmit = () => {
+    // Close the modal and start the payment process after the user information form is submitted
+    setShowModal(false);
+    handleYocoPayment();
+  };
 
-  //   stripe.redirectToCheckout({ sessionId: data.id });
-  // }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -187,16 +185,50 @@ const Cart = (props) => {
               <h3>R{totalPrice}</h3>
             </div>
             <div className="btn-container">
-            <button type="button" className="btn" id="checkout-button">
-              Pay
-            </button>
-
+              <button type="button" className="btn" id="checkout-button">
+                Pay
+              </button>
             </div>
-          </div>
+          </div>  // This closing tag was missing
         )}
-      </div>
+      <AnimatePresence>
+        {showModal && (
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            exit="hidden"
+            variants={modalVariants}
+          >
+            <div className="modal">
+              <form onSubmit={handleModalSubmit}>
+                <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+                <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                <input type="text" placeholder="Contact Number" value={contactNumber} onChange={e => setContactNumber(e.target.value)} />
+                <select value={deliveryOption} onChange={e => setDeliveryOption(e.target.value)}>
+                  <option value="pickup">In-store Pickup</option>
+                  <option value="delivery">Delivery</option>
+                </select>
+                {deliveryOption === "delivery" && (
+                  <>
+                    <select value={region} onChange={e => setRegion(e.target.value)}>
+                      <option value="">Select Region</option>
+                      <option value="gauteng">Gauteng</option>
+                      <option value="westernCape">Western Cape</option>
+                      <option value="easternCape">Eastern Cape</option>
+                      {/* Add more options as needed... */}
+                    </select>
+                    <input type="text" placeholder="Delivery Address" value={deliveryAddress} onChange={e => setDeliveryAddress(e.target.value)} />
+                  </>
+                )}
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
     </div>
   )
 }
 
-export default Cart
+export default Cart;
